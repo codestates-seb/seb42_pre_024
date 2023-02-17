77bd5a78e17a6fc5669d40a4158d5fae39035bef
KimYeongIm0 <ygim4029@gmail.com>
2023-02-17 17:24:34 +0900

    [추가] Header.js (2023.02.17 김영임)

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,129 @@
+import styled from "styled-components";
+// eslint-disable-next-line import/no-unresolved
+import logo from "../image/logo-stackoverflow.png";
+import profile from "../image/profile.png";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+
+const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  margin-top: 20px;
+  box-shadow: 0 3px 3px rgba(0, 0, 0, 0.2);
+`;
+
+const Logo = styled.a`
+  margin-left: 30px;
+  img {
+    height: 30px;
+    width: 150px;
+  }
+`;
+
+const SearchContainer = styled.div``;
+
+const SearchInput = styled.input`
+  width: 710px;
+  height: 30px;
+`;
+
+const HeaderNav = styled.nav`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  ol {
+    list-style: none;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+
+    li {
+      display: inline-block;
+    }
+  }
+`;
+
+// const LoginButton = styled.button`
+//   border: solid;
+//   border-color: #83a6c4;
+//   width: 58px;
+//   height: 30px;
+//   background-color: #e1ecf4;
+//   color: #39739d;
+//   border-radius: 7%;
+//   margin-right: 10px;
+// `;
+
+// const SignUpButton = styled.button`
+//   width: 65px;
+//   height: 30px;
+//   background-color: #0a95ff;
+//   color: #ffffff;
+//   border-radius: 7%;
+//   border: solid;
+//   border-color: rgba(67, 147, 247, 1);
+// `;
+
+const MypageButton = styled.button`
+  width: 30px;
+  height: 30px;
+  padding: 0;
+  margin: 0;
+  border: none;
+  margin-right: 10px;
+
+  img {
+    width: 30px;
+    height: 30px;
+    border-radius: 7%;
+  }
+`;
+const LogoutButton = styled.button`
+  border: solid;
+  border-color: #83a6c4;
+  width: 58px;
+  height: 30px;
+  background-color: #e1ecf4;
+  color: #39739d;
+  border-radius: 7%;
+  margin-right: 10px;
+`;
+function Header() {
+  return (
+    <HeaderContainer>
+      <Logo>
+        <img alt="logo" src={logo}></img>
+      </Logo>
+      <form>
+        <SearchContainer>
+          <FontAwesomeIcon icon={faMagnifyingGlass} />
+          <SearchInput placeholder="Search..." />
+        </SearchContainer>
+      </form>
+
+      <HeaderNav>
+        <ol>
+          <li>
+            {" "}
+            {/* <LoginButton>Log in</LoginButton> */}
+            <MypageButton>
+              <img src={profile}></img>
+            </MypageButton>
+          </li>
+          <li>
+            {" "}
+            {/* <SignUpButton>Sign up</SignUpButton> */}
+            <LogoutButton>Logout</LogoutButton>
+          </li>
+        </ol>
+      </HeaderNav>
+    </HeaderContainer>
+  );
+}
+
+export default Header;
