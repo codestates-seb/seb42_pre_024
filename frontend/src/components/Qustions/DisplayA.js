e08a54852aff5f07f1637fddebbdcd662c96fa25
KimYeongIm0 <ygim4029@gmail.com>
2023-02-20 23:01:46 +0900

    [수정] 크기조정(2023.02.20 김영임)

--- frontend/src/components/Qustions/DisplayA.js
+++ frontend/src/components/Qustions/DisplayA.js
@@ -3,17 +3,18 @@ import styled from "styled-components";
 import profile from "../../image/profile.png";
 
 const AnsTitle = styled.h2`
-  width: 80%;
-  margin-left: 100px;
+  width: 100%;
+  margin-left: 10px;
 `;
 const AnsContainer = styled.div`
   position: relative;
-  /* border: solid; */
+  /* border-top: solid; */
   /* left: 140px; */
   /* border-color: rgba(0, 0, 0, 0.3); */
   width: 100%;
   align-items: center;
-  margin-left: 100px;
+  justify-content: center;
+  /* margin-left: 100px; */
 `;
 const Answer = styled.div`
   /* left: 230px; */
@@ -21,8 +22,9 @@ const Answer = styled.div`
   border-radius: 5px;
   width: 100%;
   max-height: 300px auto;
+  margin-top: 20px;
   margin-bottom: 30px;
-  /* margin-left: 230px; */
+  margin-left: 10px;
   p {
     padding: 50px;
     font-size: 17px;
@@ -60,8 +62,7 @@ const Profile = styled.div`
   position: absolute;
   right: 0;
   margin-left: 500px;
-  min-width: 187px;
-  width: auto;
+  width: 187px;
   height: 60px;
   padding: 5px 6px 7px 7px;
   background-color: #dce9f6;
@@ -70,17 +71,12 @@ const Profile = styled.div`
   margin-bottom: 10px;
   margin-right: 50px;
   border-radius: 5px;
-  flex-wrap: wrap-reverse;
-
   img {
     width: 50px;
     height: 50px;
   }
-  .userName {
-    justify-content: center;
-  }
   b {
-    margin-left: 10px;
+    margin-left: 5px;
     color: #2e75c6;
   }
 `;
@@ -88,113 +84,6 @@ function DisplayA(props) {
   return (
     <>
       <AnsTitle>Answer</AnsTitle>
-      <AnsContainer>
-        <Answer>
-          <p>
-            position: relative 요소를 일반적인 문서 흐름에 따라 배치하고, 자기
-            자신을 기준으로 top, right, bottom, left의 값에 따라 오프셋을
-            적용합니다. 절차 : box modal -> nomal flow -> positioning position:
-            relative로 적용된 element는 static과 마찬가지로 박스 사이즈를 계산
-            후 position: relative 요소를 일반적인 문서 흐름에 따라 배치하고,
-            자기 자신을 기준으로 top, right, bottom, left의 값에 따라 오프셋을
-            적용합니다. 절차 : box modal -> nomal flow -> positioning position:
-            relative로 적용된 element는 static과 마찬가지로 박스 사이즈를 계산
-            후 position: relative 요소를 일반적인 문서 흐름에 따라 배치하고,
-            relative로 적용된 element는 static과 마찬가지로 박스 사이즈를 계산
-            후 position: relative 요소를 일반적인 문서 흐름에 따라 배치하고,
-            자기 자신을 기준으로 top, right, bottom, left의 값에 따라 오프셋을
-            적용합니다. 절차 : box modal -> nomal flow -> positioning position:
-            relative로 적용된 element는 static과 마찬가지로 박스 사이즈를 계산
-            후 position: relative 요소를 일반적인 문서 흐름에 따라 배치하고,
-            relative로 적용된 element는 static과 마찬가지로 박스 사이즈를 계산
-            후 position: relative 요소를 일반적인 문서 흐름에 따라 배치하고,
-            자기 자신을 기준으로 top, right, bottom, left의 값에 따라 오프셋을
-            적용합니다. 절차 : box modal -> nomal flow -> positioning position:
-            relative로 적용된 element는 static과 마찬가지로 박스 사이즈를 계산
-            후 position: relative 요소를 일반적인 문서 흐름에 따라 배치하고,
-          </p>
-          <ModifyWrap>
-            <Edit>Edit</Edit>
-            <Delete>Delete</Delete>
-            <Profile>
-              <img alt="logo" src={profile}></img>
-              <b>1234567890123456</b>
-            </Profile>
-          </ModifyWrap>
-        </Answer>
-      </AnsContainer>
-      <AnsContainer>
-        <Answer>
-          <p>
-            position: relative 요소를 일반적인 문서 흐름에 따라 배치하고, 자기
-            자신을 기준으로 top, right, bottom, left의 값에 따라 오프셋을
-            적용합니다. 절차 : box modal -> nomal flow -> positioning position:
-            relative로 적용된 element는 static과 마찬가지로 박스 사이즈를 계산
-            후 position: relative 요소를 일반적인 문서 흐름에 따라 배치하고,
-            자기 자신을 기준으로 top, right, bottom, left의 값에 따라 오프셋을
-            적용합니다. 절차 : box modal -> nomal flow -> positioning position:
-            relative로 적용된 element는 static과 마찬가지로 박스 사이즈를 계산
-            후 position: relative 요소를 일반적인 문서 흐름에 따라 배치하고,
-            relative로 적용된 element는 static과 마찬가지로 박스 사이즈를 계산
-            후 position: relative 요소를 일반적인 문서 흐름에 따라 배치하고,
-            자기 자신을 기준으로 top, right, bottom, left의 값에 따라 오프셋을
-            적용합니다. 절차 : box modal -> nomal flow -> positioning position:
-            relative로 적용된 element는 static과 마찬가지로 박스 사이즈를 계산
-            후 position: relative 요소를 일반적인 문서 흐름에 따라 배치하고,
-            relative로 적용된 element는 static과 마찬가지로 박스 사이즈를 계산
-            후 position: relative 요소를 일반적인 문서 흐름에 따라 배치하고,
-            자기 자신을 기준으로 top, right, bottom, left의 값에 따라 오프셋을
-            적용합니다. 절차 : box modal -> nomal flow -> positioning position:
-            relative로 적용된 element는 static과 마찬가지로 박스 사이즈를 계산
-            후 position: relative 요소를 일반적인 문서 흐름에 따라 배치하고,
-          </p>
-          <ModifyWrap>
-            <Edit>Edit</Edit>
-            <Delete>Delete</Delete>
-            <Profile>
-              <img alt="logo" src={profile}></img>
-              <div className="userName">
-                <b>dfdsgf</b>
-              </div>
-            </Profile>
-          </ModifyWrap>
-        </Answer>
-      </AnsContainer>
-      <AnsContainer>
-        <Answer>
-          <p>
-            position: relative 요소를 일반적인 문서 흐름에 따라 배치하고, 자기
-            자신을 기준으로 top, right, bottom, left의 값에 따라 오프셋을
-            적용합니다. 절차 : box modal -> nomal flow -> positioning position:
-            relative로 적용된 element는 static과 마찬가지로 박스 사이즈를 계산
-            후 position: relative 요소를 일반적인 문서 흐름에 따라 배치하고,
-            자기 자신을 기준으로 top, right, bottom, left의 값에 따라 오프셋을
-            적용합니다. 절차 : box modal -> nomal flow -> positioning position:
-            relative로 적용된 element는 static과 마찬가지로 박스 사이즈를 계산
-            후 position: relative 요소를 일반적인 문서 흐름에 따라 배치하고,
-            relative로 적용된 element는 static과 마찬가지로 박스 사이즈를 계산
-            후 position: relative 요소를 일반적인 문서 흐름에 따라 배치하고,
-            자기 자신을 기준으로 top, right, bottom, left의 값에 따라 오프셋을
-            적용합니다. 절차 : box modal -> nomal flow -> positioning position:
-            relative로 적용된 element는 static과 마찬가지로 박스 사이즈를 계산
-            후 position: relative 요소를 일반적인 문서 흐름에 따라 배치하고,
-            relative로 적용된 element는 static과 마찬가지로 박스 사이즈를 계산
-            후 position: relative 요소를 일반적인 문서 흐름에 따라 배치하고,
-            자기 자신을 기준으로 top, right, bottom, left의 값에 따라 오프셋을
-            적용합니다. 절차 : box modal -> nomal flow -> positioning position:
-            relative로 적용된 element는 static과 마찬가지로 박스 사이즈를 계산
-            후 position: relative 요소를 일반적인 문서 흐름에 따라 배치하고,
-          </p>
-          <ModifyWrap>
-            <Edit>Edit</Edit>
-            <Delete>Delete</Delete>
-            <Profile>
-              <img alt="logo" src={profile}></img>
-              <b>name</b>
-            </Profile>
-          </ModifyWrap>
-        </Answer>
-      </AnsContainer>
       <AnsContainer>
         <Answer>
           <p>
