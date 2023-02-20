0bd00e00ede023a2f99571e046e45e6978286469
KimYeongIm0 <ygim4029@gmail.com>
2023-02-21 00:51:12 +0900

    [수정]질문작성페이지 크기조절 (2023.02.20 김영임)

--- frontend/src/components/WriteQuestion.js
+++ frontend/src/components/WriteQuestion.js
@@ -1,59 +1,56 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-
+//3
 const Wrap = styled.form`
-  position: relative;
-  left: 280px;
+  width: 72%;
+  height: 100%;
   display: flex;
   flex-direction: column;
-  width: 68%;
+  position: relative;
+  left: 280px;
   margin-top: 70px;
-  margin: 0;
   padding: 30px;
 `;
-const TopTitle = styled.h1`
-  display: flex;
+const TopTitle = styled.div`
+  /* height: 70px; */
   height: 70px;
-  margin-top: 70px;
-  flex-wrap: wrap;
-  font-size: 30px;
-  flex-wrap: wrap-reverse;
+  display: flex;
+  justify-content: right;
   border-bottom: 1px solid #d6d9dc;
+  .toptitle {
+    flex: 1;
+    margin: 0;
+  }
 `;
 
 const TitleContainer = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
+  width: 80%;
   height: 150px;
   border: solid rgba(0, 0, 0, 0.2);
   border-radius: 5px;
   box-shadow: 0 3px 3px rgba(0, 0, 0, 0.2);
-  position: relative;
+  margin-top: 100px;
   color: #ffff;
-  b {
-    position: absolute;
-    top: 30px;
-    margin-right: 750px;
-    margin-bottom: 80px;
+  overflow: auto;
+  .b {
+    display: fixed;
+    margin: 30px 0 0 20px;
     font-size: 22px;
+    font-weight: bold;
     color: black;
   }
   p {
+    display: fixed;
     color: rgba(0, 0, 0, 0.5);
-    position: absolute;
-    bottom: 60px;
-    margin: 0 340px 0 0;
+    margin: 10px 0 10px 20px;
   }
 `;
 
 const TitleInput = styled.input`
-  position: absolute;
-  bottom: 15px;
-  margin: 13px 0 0 5px;
-  width: 800px;
+  margin: 5px 0 10px 20px;
+  width: 90%;
   height: 30px;
   border: solid 1px rgba(0, 0, 0, 0.5);
   border-radius: 3px;
@@ -62,40 +59,32 @@ const TitleInput = styled.input`
 const BodyContainer = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
+  width: 80%;
   height: 400px;
   border: solid rgba(0, 0, 0, 0.2);
   border-radius: 5px;
   box-shadow: 0 5px 5px rgba(0, 0, 0, 0.3);
-  position: relative;
-  margin-top: 70px;
-  b {
-    position: absolute;
-    top: 30px;
-    /* left: 55px; */
-    margin-right: 400px;
+  margin-top: 100px;
+  .b1 {
+    margin: 30px 0 10px 20px;
     font-size: 22px;
+    font-weight: bold;
     color: black;
   }
 `;
 const Body = styled.textarea`
-  position: absolute;
-  /* left: 50px; */
-  width: 800px;
-  height: 250px;
+  margin: 5px 0 10px 20px;
+  width: 90%;
+  height: 60%;
   border: solid 1px rgba(0, 0, 0, 0.5);
   border-radius: 3px;
   font-size: 20px;
 `;
 
 const Submitbtn = styled.button`
-  position: absolute;
   width: 100px;
   height: 40px;
-  /* left: 50px; */
-  bottom: 25px;
+  margin: 10px 0 0 20px;
   border: solid;
   background-color: #4393f7;
   border-color: #8ebefa;
@@ -103,7 +92,6 @@ const Submitbtn = styled.button`
   border-radius: 5px;
   cursor: pointer;
 `;
-
 function WriteQuestion() {
   //title,body,redirec
   const [btn, setBtn] = useState(false);
@@ -120,18 +108,24 @@ function WriteQuestion() {
       {/* {redirect={/}} */}
 
       <Wrap onSubmit={(e) => sendQuestion(e)}>
-        <TopTitle>Ask a public question</TopTitle>
+        <TopTitle>
+          <div className="toptitle">
+            <h1>Ask a public question</h1>
+          </div>
+        </TopTitle>
         <TitleContainer>
-          <b>Title</b>
+          <span className="b">Title</span>
           <p>
             Be specific and imagine you’re asking a question to another person.
           </p>
-          <TitleInput placeholder="  Title..." />
+          <div className="title">
+            <TitleInput placeholder="  Title..." />
+          </div>
         </TitleContainer>
 
         <BodyContainer>
-          <b>What are the details of your problem?</b>
-          <p>Introduce the problem and expand on what you put in the title.</p>
+          <span className="b1">What are the details of your problem?</span>
+          {/* <p>Introduce the problem and expand on what you put in the title.</p> */}
           <Body
             placeholder="  Introduce the problem and expand on what you put in the title "
             onClick={ClickBtn}
