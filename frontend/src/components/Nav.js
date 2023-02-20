e3c176d1616d6fc529582b425de049e7086f7d54
KimYeongIm0 <ygim4029@gmail.com>
2023-02-20 11:39:16 +0900

    [수정] nav크기 조절 (2023.02.20)

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,17 +1,18 @@
 import styled from "styled-components";
 
 const NavContainer = styled.nav`
-  width: 154px;
+  width: 280px;
   height: 100vh;
   border-right: 1px solid #d7d9dc;
-  margin: 0;
+  /* margin-left: 120px; */
   top: 70px;
   position: fixed;
 `;
 
 const NavButton = styled.button`
   margin-top: 20px;
-  width: 154px;
+  margin-left: 130px;
+  width: 150px;
   height: 33px;
   background-color: #f6f6f6;
   border: none;
