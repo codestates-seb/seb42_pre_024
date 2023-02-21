f2e1084e76367091845436a646639e5f906749d3
sdoaolo <09jieun01@naver.com>
2023-02-21 18:49:11 +0900

    [추가] Global Utils 주석(2023.02.21 강지은)

--- backend/src/main/java/com/codestates_pre024/stackoverflow/global/utils/UriMaker.java
+++ backend/src/main/java/com/codestates_pre024/stackoverflow/global/utils/UriMaker.java
@@ -5,7 +5,16 @@ import org.springframework.web.util.UriComponentsBuilder;
 import java.net.URI;
 
 public class UriMaker {
+
+    /** private constructor. Protected to prevent direct instantiation. **/
     private UriMaker() { }
+
+    /**
+     * Return the {@code URI}
+     * @param url defualt url of resource
+     * @param resourceId Primary Key of resource
+     * @return URI information of resource URI
+     */
     public static URI getUri(String url, Long resourceId) {
         String path = url+ "/" + resourceId;
         return UriComponentsBuilder.fromPath(path).build().toUri();
