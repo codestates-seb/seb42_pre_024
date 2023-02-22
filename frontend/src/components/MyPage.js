3a01f3af37d975c1292f6cf42862d6c48256f225
EunjiLee1997 <eunjilee1111@gmail>
2023-02-22 12:25:01 +0900

    [수정] MyPage.js Read 기능 1차 구현 (2023.02.22 이은지)

--- frontend/src/components/MyPage.js
+++ frontend/src/components/MyPage.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
-import profile from "../image/profile.png";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Wrap = styled.main`
   width: 72%;
@@ -22,16 +24,16 @@ const ProfileContainer = styled.form`
   flex-direction: row;
   text-align: center;
   .imgContainer {
-    width: 150px;
-    height: 150px;
+    width: 130px;
+    height: 130px;
     > img {
-      width: 100%;
-      height: 100%;
+      width: 130px;
+      height: 130px;
       border-radius: 4px;
     }
   }
   .userInfoContainer {
-    width: 600px;
+    width: 100%;
     display: flex;
     flex-direction: column;
     text-align: left;
@@ -50,6 +52,10 @@ const MyQuestionsContainer = styled.div`
     border-radius: 4px;
     height: 300px;
     padding: 30px;
+    > ul {
+      list-style: none;
+      padding-left: 0px;
+    }
   }
 `;
 
@@ -67,25 +73,60 @@ const MyPageButton = styled.button`
 `;
 
 function MyPage() {
+  const [myProfile, setMyProfile] = useState();
+
+  const readMyProfile = async () => {
+    const { data } = await axios.get("http://localhost:4000/members");
+    setMyProfile(data);
+  };
+
+  useEffect(() => {
+    (async () => {
+      await readMyProfile();
+    })();
+  }, []);
+
   return (
     <Wrap>
-      <ProfileContainer>
-        <div className="imgContainer">
-          <img alt="profile_image" src={profile}></img>
-        </div>
-        <div className="userInfoContainer">
-          <h1>User Name</h1>
-          <div>about me 내용</div>
-        </div>
-        <div className="buttonContainer">
-          <MyPageButton>Edit my profile</MyPageButton>
-        </div>
-      </ProfileContainer>
-      <MyQuestionsContainer>
-        <h1>My Questions</h1>
-        <div>질문 목록</div>
-      </MyQuestionsContainer>
-      <MyPageButton>탈퇴하기</MyPageButton>
+      {myProfile && (
+        <>
+          <ProfileContainer>
+            <div className="imgContainer">
+              <img alt="profile_image" src={myProfile.profileImage}></img>
+            </div>
+            <div className="userInfoContainer">
+              <h1>{myProfile.name}</h1>
+              <div>{myProfile.aboutMe}</div>
+            </div>
+            <div className="buttonContainer">
+              <MyPageButton>Edit my profile</MyPageButton>
+            </div>
+          </ProfileContainer>
+          <MyQuestionsContainer>
+            <h1>My Questions</h1>
+            <div>
+              {myProfile.questions.length === 0 && (
+                <p>작성한 질문이 없습니다.</p>
+              )}
+              {myProfile.questions &&
+                myProfile.questions.map((question) => {
+                  return (
+                    <ul>
+                      <li key={question.id}>
+                        <Link
+                          to={`http://localhost:3000/questions/${question.id}`}
+                        >
+                          {question.title}
+                        </Link>
+                      </li>
+                    </ul>
+                  );
+                })}
+            </div>
+          </MyQuestionsContainer>
+          <MyPageButton>탈퇴하기</MyPageButton>
+        </>
+      )}
     </Wrap>
   );
 }
