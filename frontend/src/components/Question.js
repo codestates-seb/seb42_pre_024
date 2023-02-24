bb9da0b3054afeb8a498b2f112019c38a3a5738c
KimYeongIm0 <ygim4029@gmail.com>
2023-02-24 17:10:01 +0900

    [수정] post,patch 시도중(2023.02.24 김영임)

--- frontend/src/components/Question.js
+++ frontend/src/components/Question.js
@@ -5,6 +5,7 @@ import { useSelector, useDispatch } from "react-redux";
 import DisplayQ from "./Qustions/DisplayQ";
 import DisplayA from "./Qustions/DisplayA";
 import WriteAns from "./Qustions/WriteAns";
+import { useParams } from "react-router-dom";
 
 const Container = styled.div`
   position: relative;
@@ -17,22 +18,25 @@ const Container = styled.div`
   padding: 30px;
 `;
 
-function Question(props) {
+function Question() {
   //props {match} ,
   //get요청시 match.params.id , setQustion
   //question(s)
   //로그인x=>답변불가 질문불가,본인질문x=>답변ㅇ 질문가능,본인질문=>질문수정 질문가능,본인이 답변=>질문수정x 답변가능
   //로컬스토리지 이용해서 수정,삭제버튼 나오게하고 수정하거나 삭제 action시 서버에 반영??
+
   const [list, setList] = useState();
   let { id } = useSelector((state) => {
     return state.paramsId;
   });
-  console.log(id);
+
   const readData = async () => {
-    const { data } = await axios.get(`http://localhost:4000/data/${id}`);
-    setList([data]);
-    //나중에 서버랑 찐 통신할 때는 배열 빼기
+    const { data } = await axios.get(
+      `http://ec2-3-36-122-3.ap-northeast-2.compute.amazonaws.com:8080/questions/${id}`
+    );
+    setList([data.data]);
   };
+
   useEffect(() => {
     (async () => {
       await readData();
