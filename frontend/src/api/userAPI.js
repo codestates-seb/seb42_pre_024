a56b1ed071026a3fe69d388cf121817d733f908d
EunjiLee1997 <eunjilee1111@gmail>
2023-02-28 13:27:10 +0900

    [리팩토링] 로그인 관련 기능 리팩토링 (2023.02.28 이은지)

--- frontend/src/api/userAPI.js
+++ frontend/src/api/userAPI.js
@@ -15,12 +15,36 @@ export const login = async (data) => {
   }
 };
 
+export const logout = async () => {
+  try {
+    const res = await axios({
+      method: "post",
+      url: "/logout",
+    });
+    return res;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
 export const signUp = async (data) => {
   try {
     const res = await axios({
       method: "post",
       data,
       url: "/login",
+    });
+    return res;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const deleteAccount = async (memberId) => {
+  try {
+    const res = await axios({
+      method: "delete",
+      url: `$members/${memberId}`,
     });
     return res;
   } catch (e) {
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,6 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
 import questionSlice from "./questionSlice";
-import userSlice from "./userSlice";
 import editSlice from "./editSlice";
 import paramsIdSlice from "./paramsId.Slice";
 import userIdSlice from "./userIdSlice";
@@ -8,7 +7,6 @@ import userIdSlice from "./userIdSlice";
 const store = configureStore({
   reducer: {
     question: questionSlice,
-    user: userSlice,
     edit: editSlice,
     paramsId: paramsIdSlice,
     userId: userIdSlice,
